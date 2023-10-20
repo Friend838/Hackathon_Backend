27f0b58dd83482ee7294692b7914638c296c48e9
Friend838 <azsx9015223@gmail.com>
2023-10-20 06:28:56 -0700

    finish weekly report post

--- api-server/main.py
+++ api-server/main.py
@@ -7,12 +7,14 @@ from src.config.config import Settings
 from src.controller.employee.endpoint import employee_router
 from src.controller.enter_record.endpoint import enter_record_router
 from src.controller.machine_record.endpoint import machine_record_router
+from src.controller.weekly_report.endpoint import weekly_report_router
 from src.dependencies.settings import get_settings
 
 app = FastAPI()
 app.include_router(employee_router)
 app.include_router(enter_record_router)
 app.include_router(machine_record_router)
+app.include_router(weekly_report_router)
 
 
 @app.get("/")
