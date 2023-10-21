88dd806f580fd770f83e087222cef18bcf6dc61c
Friend838 <azsx9015223@gmail.com>
2023-10-21 09:48:30 -0700

    reset cache

--- api-server/src/controller/enter_record/schema/query_enter_record.py
+++
@@ -1,9 +0,0 @@
-from camel_converter import dict_to_camel
-from camel_converter.pydantic_base import CamelBase
-
-
-class QueryEnterRecord(CamelBase):
-    employee_id: str
-    zone: str
-    shift_time: str
-    status: str
--- api-server/src/dependencies/settings.py
+++
@@ -1,9 +0,0 @@
-from functools import lru_cache
-
-# pylint: disable=import-error
-from src.config.config import Settings
-
-
-@lru_cache()
-def get_settings():
-    return Settings()
