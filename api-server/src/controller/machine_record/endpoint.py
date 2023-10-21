7fbe9154a85503d0c3b23d58e0774202b5b8232b
Friend838 <azsx9015223@gmail.com>
2023-10-20 19:18:21 -0700

    add query machine records

--- api-server/src/controller/machine_record/endpoint.py
+++ api-server/src/controller/machine_record/endpoint.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from typing import Annotated
 
 # pylint: disable=import-error
 from src.controller.machine_record.schema.post_machine_record import (
     PostMachineRecordRequestBody,
     PostMachineRecordResponseBody,
 )
+from src.controller.machine_record.schema.query_machine_record import QueryMachineRecord
 from src.service.machine_record_service import MachineRecordService
 
 service = MachineRecordService()
@@ -23,3 +25,14 @@ def generate_machine_record(
     body: PostMachineRecordRequestBody,
 ):
     return service.post_machine_record(body)
+
+
+@machine_record_router.get(
+    path="/",
+    response_model=list[QueryMachineRecord],
+)
+def query_machine_record(
+    start_timestamp: Annotated[int, Query(gt=0, example=None)],
+    end_timestamp: Annotated[int, Query(gt=0, example=None)],
+):
+    return service.get_machine_record(start_timestamp, end_timestamp)
